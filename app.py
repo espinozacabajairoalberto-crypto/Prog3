fba65a971f61c3bf4e3508663b3f23a130250193
JairoAEC <espinozacabajairoalberto@gmail.com>
2026-04-21 16:31:12 -0400

    version 3

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='canciones top de la semana')
+    lista = ["la guitarra", "para no verte mas" , "balada para un gordo"]
+    return render_template('lista.html', titulo='canciones top de la semana', musicas = lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
